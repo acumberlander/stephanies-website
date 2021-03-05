8a35e0fa3e3200c82e5624a3e22199faf425771e
Austin Cumberlander <austincumberlander@gmail.com>
2021-03-04 19:41:04 -0600

    work in progress for shop page.

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Container, Grow, Grid, Card } from '@material-ui/core';
+import { Container, Grow, Grid, Card, Typography } from '@material-ui/core';
 import Product from '../Products/Product/Product.jsx';
 import './Shop.scss';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+	header: {
+		textAlign: 'center',
+		margin: '70px 0',
+	},
+}));
 
 const Shop = () => {
+	const { header } = useStyles();
+
 	return (
 		<Container>
+			<Typography className={header}>
+				<h1>All Products</h1>
+			</Typography>
 			<Product />
 		</Container>
 	);
