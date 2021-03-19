e1345500d6c2bcd310d159c3464337799539100c
Austin Cumberlander <austincumberlander@gmail.com>
2021-03-18 20:37:34 -0500

    Fixed issue with carousel adding margin to the right side of the app container.

--- src/components/HomeComponents/Carousel/Carousel.js
+++ src/components/HomeComponents/Carousel/Carousel.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import HeroCarousel from 'react-hero-carousel';
+import { Link } from 'react-router-dom';
+import whiteLogo from '../../../assets/logos/logo-white.png';
+
+import HeroSlider, { Slide, Nav, OverlayContainer } from 'hero-slider';
+
 import stephWithSmoke from '../../../assets/steph-smoke.jpg';
 import stephLaid from '../../../assets/steph-laid.jpg';
 import stephWithGlass from '../../../assets/glassware/steph-and-glassware.jpg';
@@ -10,36 +14,64 @@ const Carousel = () => {
 	const classes = useStyles();
 
 	return (
-		<HeroCarousel className={classes.heroCarousel} interval={3000}>
-			<img
-				className={classes.stephSmile}
-				src={steph2}
-				alt="sexes pic"
-				width="100%"
-				height="100%"
-			/>
-			<img
-				className={classes.carouselImage}
-				src={stephWithGlass}
-				alt="sexes pic"
-				width="100%"
-				height="100%"
-			/>
-			<img
-				className={classes.carouselImage}
-				src={stephLaid}
-				alt="sexes pic"
-				width="100%"
-				height="100%"
-			/>
-			<img
+		<HeroSlider
+			slidingAnimation="fade"
+			orientation="horizontal"
+			initialSlide={1}
+			style={{
+				backgroundColor: 'rgba(0, 0, 0, 0.33)',
+			}}
+			settings={{
+				sliderFadeInDuration: 100,
+				navbarFadeInDuration: 1000,
+				navbarFadeInDelay: 500,
+				shouldAutoplay: true,
+				shouldDisplayButtons: false,
+				autoplayDuration: 3000,
+				height: '100vh',
+			}}
+		>
+			<OverlayContainer>
+				<div className={classes.carouselContainer}>
+					<div className={classes.carouselText}>
+						<img className={classes.homeLogo} src={whiteLogo} alt="logo" />
+						<h3 className={classes.carouselSubtitle}>By Stephanie</h3>
+						<div className={classes.linkContainer}>
+							<Link className={classes.shopLink} to="/shop/all-products">
+								<h3 className={classes.linkText}>Shop Now</h3>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</OverlayContainer>
+
+			<Slide
 				className={classes.stephWithSmoke}
-				src={stephWithSmoke}
-				alt="sexes pic"
-				width="100%"
-				height="100%"
+				background={{
+					backgroundImage: stephWithSmoke,
+					backgroundPosition: '50% 40%',
+				}}
 			/>
-		</HeroCarousel>
+			<Slide
+				className={classes.stephWithGlass}
+				background={{
+					backgroundImage: stephWithGlass,
+					backgroundPosition: '53% 40%',
+				}}
+			/>
+			<Slide
+				className={classes.stephSmile}
+				background={{ backgroundImage: steph2, backgroundPosition: '60% 70%' }}
+			/>
+			<Slide
+				className={classes.stephLaid}
+				background={{
+					backgroundImage: stephLaid,
+					backgroundPosition: '10% 50%',
+				}}
+			/>
+			<Nav />
+		</HeroSlider>
 	);
 };
 
