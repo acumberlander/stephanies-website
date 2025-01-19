24b32b76bfb3a0c536b23a5de61c540c9c44591e
Austin <austincumberlander@gmail.com>
2025-01-18 18:06:35 -0600

    work in progress

--- src/CSS/colors.js
+++
@@ -1,8 +0,0 @@
-export const colors = {
-	primary: '#cc34ab',
-	primaryFaded: 'rgba(204,52,171, 0.65)',
-	primaryHover: '#b32093',
-	danger: 'red',
-	black: 'black',
-	white: 'white',
-};
--- src/lib/commerce.js
+++
@@ -1,7 +0,0 @@
-import Commerce from "@chec/commerce.js";
-
-const publicKey = "pk_test_24256647a72235a632d8eafec3ffe6d9e3b08d54e47bd";
-
-const commerce = new Commerce(publicKey, true);
-
-export default commerce;
