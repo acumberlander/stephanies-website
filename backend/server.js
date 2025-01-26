ea9c5324488b4e4dd1ec23c66750964c62c9ed07
Austin <austincumberlander@gmail.com>
2025-01-25 18:42:51 -0600

    Updates

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,7 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Connect to Mongo
